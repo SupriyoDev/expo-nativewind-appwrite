c9d897ce9d5209e86a879dfa76e48f9c6a5d23bc
supriyo maji <supriyamaji.math@gmail.com>
2025-07-02 21:10:25 +0530

    updated dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,12 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <SafeAreaView className="h-full w-full bg-white">
       <Text className="text-green-600 font-bold text-3xl text-center">
         Edit app/index.tsx to edit this screen.{" "}
       </Text>
-    </View>
+    </SafeAreaView>
   );
 }
